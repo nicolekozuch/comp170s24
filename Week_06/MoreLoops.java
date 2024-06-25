e49e864e5fa1a3b0d5f2e74da73c7cc7eef69ff0
Leo Irakliotis <lgreco@gmail.com>
2024-06-25 17:16:35 +0000

    w6 tue isWord

--- Week_06/MoreLoops.java
+++ Week_06/MoreLoops.java
@@ -16,4 +16,30 @@ public class MoreLoops {
         }
         return counter;
     }
+
+    /**
+     * Tell if a given string is a word or not
+     */
+    static boolean isWord(String string) {
+        int ascii_A = (int) 'A';
+        int ascii_a = (int) 'a';
+        int ascii_Z = (int) 'Z';
+        int ascii_z = (int) 'z';
+        boolean result = false;
+        if (string != null && string.length() > 0) {
+            // check out its contents one character at a time, ensuring that
+            // each and every character is a letter, A-Z or a-z
+            int position = 0;
+            result = true;
+            while (position < string.length() && result) {
+                // The moment I find a non letter character, change result to false and
+                // end the loop. When a character is NOT a letter
+                int current = (int) string.charAt(position); // ASCII of current character
+                if (current < ascii_A) { result = false; } // less than A
+                if (current > ascii_z) { result = false; } // more than z
+                if (ascii_Z < current && current < ascii_a) { result = false; } // between Z-a
+            }
+        }
+        return result;
+    }
 }
