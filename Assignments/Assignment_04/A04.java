037bfaf66c43e7039a521b8de42629d0d1757587
nicolekozuch <122937900+nicolekozuch@users.noreply.github.com>
2024-06-26 04:29:29 +0000

    first draft, need to add comments

--- Assignments/Assignment_04/A04.java
+++ Assignments/Assignment_04/A04.java
@@ -4,5 +4,28 @@ public class A04 {
         printEvenCharacters("Springfield"); // Expect: S r n f e d 
     } // method main
 
+    // declare printOddCharacters() method
+    public static void printOddCharacters(String odd) {
+        System.out.println(); // stylistic choice
+        char oddChar;
+        for (int i = 0; i < odd.length(); i++) {
+            if (i % 2 != 0) {
+                oddChar = odd.charAt(i);
+                System.out.print(oddChar + " ");
+            }
+        }
+        System.out.println();
+    }
 
+    // declare printEvenCharacters() method
+    public static void printEvenCharacters(String even) {
+        char evenChar;
+        for (int i = 0; i < even.length(); i++) {
+            if (i % 2 == 0) {
+                evenChar = even.charAt(i);
+                System.out.print(evenChar + " ");
+            }
+        }
+        System.out.println("\n"); // stylistic choice
+    }
 }
