01ffbc62deb2fd8bcfd800a212bfa1d3c086e2d6
nicolekozuch <122937900+nicolekozuch@users.noreply.github.com>
2024-06-26 17:30:23 +0000

    hw A04 completed

--- Assignments/Assignment_04/A04.java
+++ Assignments/Assignment_04/A04.java
@@ -4,28 +4,31 @@ public class A04 {
         printEvenCharacters("Springfield"); // Expect: S r n f e d 
     } // method main
 
-    // declare printOddCharacters() method
+    /* 
+    declare printOddCharacters() method that accepts a String as a parameter
+    prints out the characters found at odd indexes
+    */
     public static void printOddCharacters(String odd) {
-        System.out.println(); // stylistic choice
-        char oddChar;
+        // for loop goes through each character in the given string
         for (int i = 0; i < odd.length(); i++) {
-            if (i % 2 != 0) {
-                oddChar = odd.charAt(i);
-                System.out.print(oddChar + " ");
+            if (i % 2 == 1) { // equation to find characters at odd indexes
+                System.out.print(odd.charAt(i) + " ");
             }
         }
-        System.out.println();
+        System.out.println(); // new line places next output on separate line
     }
 
-    // declare printEvenCharacters() method
+    /* 
+    declare printEvenCharacters() method that accepts a String as a parameter
+    prints out the characters found at even indexes, including zero
+    */
     public static void printEvenCharacters(String even) {
-        char evenChar;
+        // for loop goes through each character in the given string
         for (int i = 0; i < even.length(); i++) {
-            if (i % 2 == 0) {
-                evenChar = even.charAt(i);
-                System.out.print(evenChar + " ");
+            if (i % 2 == 0) { // equation to find characters at even indexes
+                System.out.print(even.charAt(i) + " ");
             }
         }
-        System.out.println("\n"); // stylistic choice
+        System.out.println(); // new line places next output on separate line
     }
 }
