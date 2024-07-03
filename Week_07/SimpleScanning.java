5e1729cf149d94296e3334d1cf982a6b4c1f0808
Leo Irakliotis <lgreco@gmail.com>
2024-07-03 17:18:46 +0000

    w7 scanner

--- Week_07/SimpleScanning.java
+++ Week_07/SimpleScanning.java
@@ -1,16 +1,33 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class SimpleScanning {
-    public static void main(String[] args) throws FileNotFoundException {
-        File file = new File("Week_07/students.md");
-        Scanner scanner = new Scanner(file);
-        while (scanner.hasNext()) {
-            String name = scanner.next();
-            String email = scanner.next();
-            System.out.println("Name: "+name+", email: " + email);
+
+    static int countLines(String filename) throws FileNotFoundException {
+        File file = new File(filename);
+        Scanner sc = new Scanner(file);
+        int lineCount = 0;
+        while (sc.hasNextLine()) {
+            lineCount++;
+            sc.nextLine(); // carriage return --- move to next line in file
         }
-        scanner.close();
+        sc.close(); // be nice Leo
+        return lineCount;
+    } // method countLines
+
+    public static void main(String[] args) throws FileNotFoundException {
+        ArrayList<String> names = new ArrayList<>();
+        ArrayList<String> emails = new ArrayList<>();
+        File file = new File("Week_07/students.txt");
+        Scanner sc = new Scanner(file);
+        while (sc.hasNext()) {
+            names.add(sc.next());
+            emails.add(sc.next());
+        }
+        System.out.println(names);
+        System.out.println(emails);
     }
 }
